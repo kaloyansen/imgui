8c3e6a6e789e5f36f6fb7cc6ae93dccef66fc7f5
kaloyanski <kaloyan@triplehelix-consulting.com>
2024-02-11 11:02:33 +0200

    ignore eclipse artifacts

--- src/sinfo/main.cpp
+++ src/sinfo/main.cpp
@@ -15,6 +15,7 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl2.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <iostream>
 #include <fstream>
@@ -49,11 +50,44 @@ std::vector<std::string> split(const std::string& s, char delimiter) {
      std::vector<std::string> tokens;
      std::string token;
      std::istringstream tokenStream(s);
-     while (std::getline(tokenStream, token, delimiter)) {
-          tokens.push_back(token);
-     }
+     while (std::getline(tokenStream, token, delimiter))
+          if (!token.empty())
+               tokens.push_back(token);
+
      return tokens;
 }
+
+
+int getDirectiveValue(const char* path, const char* directive)
+{
+     FILE* file = fopen(path, "r");
+     if (file == NULL) return -1;
+
+     char line[256];
+     double valeur = -1.0;
+
+     while (fgets(line, sizeof(line), file) != NULL) {
+          if (strstr(line, directive)) {
+               char* token = strtok(line, ":");
+               if (token != NULL) {
+                    token = strtok(NULL, ":");
+                    if (token != NULL) {
+                         valeur = atof(token);
+                         break;
+                    }
+               }
+          }
+     }
+
+     fclose(file);
+
+     return valeur;
+}
+
+
+
+
+
 
 
 double getCPUUsage() {
@@ -66,31 +100,27 @@ double getCPUUsage() {
      statFile.close();
 
      // Split the line into tokens
-     std::vector<std::string> tokens = split(line, ' ');
+     std::vector<std::string> token = split(line, ' ');
 
      // Calculate total CPU time
      long long totalCPUTime = 0;
 
-     for (size_t i = 1; i < tokens.size(); ++i) {
-          if (!tokens[i].empty())
+     for (size_t i = 1; i < token.size(); ++i)
+     {
+          try
           {
-               try {
-                    long long value = std::stoll(tokens[i]);
-                    totalCPUTime += value;
-               } catch (const std::exception& e) {
-                    std::cerr << "Error converting token to long long: " << e.what() << std::endl;
-                    std::cerr << "Token content: " << tokens[i] << std::endl;
-               }
+               long long value = std::stoll(token[i]);
+               totalCPUTime += value;
+          }
+          catch (const std::exception& e)
+          {
+               glfw_error_callback(1, "error converting token to long long: ");
+//               glfw_error_callback(1, token[i]);
           }
      }
 
-     /*   for (size_t i = 1; i < tokens.size(); ++i) {
-          totalCPUTime += std::stoll(tokens[i]);
-          }
-     */
-
      // Calculate idle CPU time
-     long long idleCPUTime = std::stoll(tokens[4]);
+     long long idleCPUTime = std::stoll(token[4]);
 
      // Calculate CPU usage
      double cpuUsage = 100.0 * (1.0 - static_cast<double>(idleCPUTime) / totalCPUTime);
@@ -99,12 +129,29 @@ double getCPUUsage() {
 }
 
 
-
-void updateMemoryInfo(long long* totalMem, long long* freeMem, long long* totalSpace, long long* freeSpace,
-                      double* loadAvg, std::string* loadLine, double* cpUsage)
+void getState(const char* path, std::string* data)
 {
-//	*cpUsage =
-     getCPUUsage();
+     FILE* file = fopen(path, "r");
+     if (file == NULL) return;
+
+     char line[256];
+     fgets(line, sizeof(line), file);
+     *data = line;
+     fclose(file);
+     
+
+     // std::ifstream file(path);
+     // if (!file.is_open()) return;
+     // std::getline(file, *data);
+}
+
+
+void updateSystemInfo(long long* totalMem, long long* freeMem,
+                      long long* totalSpace, long long* freeSpace,
+                      double* loadavgarr, float* cpufreq,
+                      std::string* procloadavg, std::string* procstat)
+{
+     *cpufreq = getDirectiveValue("/proc/cpuinfo", "cpu MHz");//getCPUUsage();
 
      struct sysinfo memInfo;
      sysinfo(&memInfo);
@@ -117,32 +164,39 @@ void updateMemoryInfo(long long* totalMem, long long* freeMem, long long* totalS
           *totalSpace = 100;
           *freeSpace = 1;
 
-     } else
-     {
+     } else {
+          
           *totalSpace = vfs.f_blocks * vfs.f_frsize;
           *freeSpace = vfs.f_bfree * vfs.f_frsize;
      }
 
 
-     std::ifstream loadFile("/proc/loadavg");
-     if (!loadFile.is_open()) return;
-     //std::cerr << "Error: Failed to open /proc/loadavg.\n";
-     std::getline(loadFile, *loadLine);
-     std::istringstream iss(*loadLine);
-     for (int i = 0; i < 7; ++i) iss >> loadAvg[i];
+     
+     // std::ifstream loadFile("/proc/loadavg");
+     // if (!loadFile.is_open()) return;
+     // std::getline(loadFile, *loadLine);
+     getState("/proc/stat", procstat);
+     getState("/proc/loadavg", procloadavg);
+
+
+     std::istringstream iss(*procloadavg);
+     for (int i = 0; i < 7; ++i) iss >> loadavgarr[i];
 
 }
 
-void loaData(float (*plotData)[], int arrSize, float newData)
+void loaData(float plotData[], int arrSize, float newData)
 {
-     for (int i = 0; i < arrSize; i++)
-     {
-          (*plotData)[i - 1] = (*plotData)[i];
-     }
-     (*plotData)[arrSize - 1] = newData;
+     for (int i = 0; i < arrSize; i++) plotData[i] = plotData[i + 1];
+     plotData[arrSize - 1] = newData;
 }
 
-
+float* randoBuffer(int size)
+{
+     float* buffer = new float[size];
+     for (int i = 0; i < size; i++)
+          buffer[i] = static_cast<float>(rand()) / RAND_MAX;
+     return buffer;
+}
 
 //
 // Main code
@@ -203,17 +257,30 @@ int main(int, char**)
 
 
      long long totalMem, freeMem, totalSpace, freeSpace;
-     double loadAvg[7] = {0.0};
-     double cpUsage;
-     std::string loadLine;
+     double loadavgarr[7] = {0.0};
+     float cpufreq;
+     std::string procloadavg;
+     std::string procstat;
      int procimax = 0;
-     int plotLineSize = 333;
-     float plotData[plotLineSize];
+     int bufSize = 333;
+     float bufloadavg0[bufSize];
+     float bufloadavg1[bufSize];
+     float bufloadavg2[bufSize];
+     float bufloadavg3[bufSize];
+     float* bufcpufreq = randoBuffer(bufSize);
+     float* bufappfreq = randoBuffer(bufSize);
+     float* bufupfreq = randoBuffer(bufSize);
 
+     int loop = 0;
+     int uloop = 0;
+     const char* rose = "calibrate";
+     int delay = 1;
+     float fontScale = 2.0f;
 
      // Main loop
      while (!glfwWindowShouldClose(window))
      {
+          loop++;
           // Poll and handle events (inputs, window resize, etc.)
           // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
           // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
@@ -232,81 +299,165 @@ int main(int, char**)
 
           // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
           {
-               //static float f = 0.0f;
-               static int counter = 0;
-               if (!do_not_update_system_info) updateMemoryInfo(&totalMem, &freeMem, &totalSpace, &freeSpace, loadAvg, &loadLine, &cpUsage);
+               do_not_update_system_info = loop % delay ? true : false;
+               if (!do_not_update_system_info)
+               {
+                    uloop++;
+                    updateSystemInfo(&totalMem, &freeMem, &totalSpace, &freeSpace, loadavgarr, &cpufreq,  &procloadavg,  &procstat);
+               }
 
+               float appfreq = 1.0f / io.DeltaTime;
+               float upfreq = appfreq / delay;
+                              
                float ram = 100 * float(freeMem) / float(totalMem);
                float rom = 100 * float(freeSpace) / float(totalSpace);
-               int proci = int(loadAvg[3]);
+               int proci = int(loadavgarr[3]);
                procimax = proci > procimax ? proci : proci * 2;
+               //if (counter > 0) rose = "yeah, thank you";
 
-               
-               // if (!do_not_update_system_info) loaData(&plotData,
-               //                                         plotLineSize,
-               //                                         loadAvg[3]);
-
-               for (int i = 0; i < IM_ARRAYSIZE(plotData); i++)
+               if (!do_not_update_system_info)
                {
-                    plotData[i - 1] = plotData[i];
+                    loaData(bufloadavg0, bufSize, loadavgarr[0]);
+                    loaData(bufloadavg1, bufSize, loadavgarr[1]);
+                    loaData(bufloadavg2, bufSize, loadavgarr[2]);
+                    loaData(bufloadavg3, bufSize, loadavgarr[3]);
+                    loaData(bufcpufreq, bufSize, cpufreq);
+                    loaData(bufappfreq, bufSize, appfreq);
+                    loaData(bufupfreq, bufSize, upfreq);
                }
-               plotData[plotLineSize - 1] = loadAvg[3];
 
-               ImGui::Begin("main");
-               ImGui::Indent();
+               // for (int i = 0; i < IM_ARRAYSIZE(plotData); i++)
+               // {
+               //      plotData[i - 1] = plotData[i];
+               // }
+               // plotData[bufSize - 1] = loadavgarr[3];
+
+               io.FontGlobalScale = fontScale;
+
+               static ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings |  ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar;
+               
+               const ImGuiViewport* viewport = ImGui::GetMainViewport();
+               ImGui::SetNextWindowPos(viewport->WorkPos);
+               ImGui::SetNextWindowSize(viewport->WorkSize);
+
+               bool boopen = true;
+               bool* poopen = &boopen;
+
+               ImGui::Begin("main", poopen, flags);
 //               ImGui::CheckboxFlags("ImGuiWindowFlags_NoTitleBar", &flags, ImGuiWindowFlags_NoTitleBar);
 //               ImGui::CheckboxFlags("ImGuiWindowFlags_NoCollapse", &flags, ImGuiWindowFlags_NoCollapse);
 //               ImGui::CheckboxFlags("ImGuiWindowFlags_NoScrollbar", &flags, ImGuiWindowFlags_NoScrollbar);
-               ImGui::Text("under construction");
-               ImGui::Checkbox("pause", &do_not_update_system_info);
-               ImGui::Checkbox("Demo Window", &show_demo_window);
-               ImGui::Checkbox("Another Window", &show_another_window);
-               ImGui::ColorEdit3("clear color", (float*)&clear_color);
+               ImGui::SliderFloat("scale", &fontScale, 0.5, 5);
+               ImGui::ShowStyleSelector("style");
+               ImGui::SliderInt("delay", &delay, 1, 10);
+               ImGui::Separator();
+
+               //ImGui::Checkbox("pause", &do_not_update_system_info);
+               //ImGui::ColorEdit3("clear color", (float*)&clear_color);
 
 /* buttons return true when clicked like most widgets when edited or activated */
-               if (ImGui::Button("Button")) counter++;
+               
+               //ImGui::SliderFloat("RAM", &ram, 0.0f, 100.0f);
+               //ImGui::SameLine();
+               //ImGui::Text("my counter is %d", counter);
 
+               //ImGui::SliderFloat("frequence, Hz", &frequence, 1, 120);
+               ImGui::ProgressBar(upfreq / 100, ImVec2(0.0f, 0.0f));
                ImGui::SameLine();
-               if (counter > 3)
+               ImGui::Text("update frequence %.1f Hz", upfreq);
+               ImGui::ProgressBar(ram / 100, ImVec2(0.0f, 0.0f));
+               ImGui::SameLine();
+               ImGui::Text("RAM %d/%d Mo", int(freeMem / 1024 / 1024), int(totalMem / 1024 / 1024));
+
+               //ImGui::SliderFloat("ROM", &rom, 0.0f, 100.0f);
+               ImGui::ProgressBar(rom / 100, ImVec2(0.0f, 0.0f));
+               ImGui::SameLine();
+               ImGui::Text("ROM %d/%d Go", int(freeSpace / 1024 / 1024 / 1024), int(totalSpace / 1024 / 1024 /1024));
+
+               ImGui::Separator();
+               if (ImGui::BeginTable("split", 5, ImGuiTableFlags_Resizable | ImGuiTableFlags_NoSavedSettings))
                {
-                    counter = 0;
-               } 
-               else if (counter > 0)
-               {
-                    ImGui::Text("yeah, thank you");
+                    //sprintf(buf, "%03d", i);
+                    ImGui::TableNextColumn();
+                    ImGui::Text("app avg rate, s");
+                    ImGui::TableNextColumn();
+                    ImGui::Text("FPS");
+                    ImGui::TableNextColumn();
+                    ImGui::Text("updates");
+                    ImGui::TableNextColumn();
+                    ImGui::Text("loops");
+                    ImGui::TableNextColumn();
+                    ImGui::Text("loops/60");
+                    ImGui::TableNextRow();
+                    ImGui::TableNextColumn();
+                    ImGui::Text("%.6f", 1.0f / io.Framerate);
+                    ImGui::TableNextColumn();
+                    ImGui::Text("%.1f", io.Framerate);
+                    ImGui::TableNextColumn();
+                    ImGui::Text("%d", uloop);
+                    ImGui::TableNextColumn();
+                    ImGui::Text("%d", loop);
+                    ImGui::TableNextColumn();
+                    ImGui::Text("%d", loop / 60);
+                    ImGui::EndTable();
                }
-               else
-               {
-                    ImGui::Text("touch me, please");
-               }
 
+               ImGui::SliderInt("processed", &proci, 0, procimax);
+               // if (uloop > bufSize)
+               // {
+               ImGui::SeparatorText("ImGui::SeparaorText");
+               if (ImGui::Button(rose)) uloop = 0;
+
+               char overlay[32];
+               sprintf(overlay, "%12.0f Hz", cpufreq * 1000 * 1000);
+               //ImGui::SetNextItemHeight(ImGui::GetFontSize() * 8);
+               ImGui::PlotLines("core frequence", bufcpufreq, bufSize, 0, overlay);
+
+               ImGui::PlotLines("imgui frequence", bufappfreq, bufSize);
                ImGui::SameLine();
-               ImGui::Text("my counter is %d", counter);
-               ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+               ImGui::Text("%7.1f Hz", appfreq);
 
-               ImGui::SliderFloat("RAM", &ram, 0.0f, 100.0f);
+               ImGui::PlotLines("update frequence", bufupfreq, bufSize);
                ImGui::SameLine();
-               ImGui::Text("%d/%d Mo", int(freeMem / 1024 / 1024), int(totalMem / 1024 / 1024));
+               ImGui::Text("%7.1f Hz", upfreq);
+                    
+               ImGui::PlotLines("current load", bufloadavg3, bufSize);
+               ImGui::PlotLines("01 min load avg", bufloadavg0, bufSize);
+               ImGui::PlotLines("05 min load avg", bufloadavg1, bufSize);
+               ImGui::PlotLines("15 min load avg", bufloadavg2, bufSize);
+               // }
+               // else
+               // {
+               //      int progress = 100 - (100 * float(bufSize - uloop) / bufSize);
+               //      ImGui::Text("loading ...");
+               //      ImGui::Text("calibrate process load");
+               //      ImGui::SameLine();
+               //      ImGui::ProgressBar(float(progress)/100, ImVec2(0.0f, 0.0f));
+               //      ImGui::Text("calculate one min average");
+               //      ImGui::SameLine();
+               //      ImGui::ProgressBar(float(progress)/100, ImVec2(0.0f, 0.0f));
+               //      ImGui::Text("calculate five min average");
+               //      ImGui::SameLine();
+               //      ImGui::ProgressBar(float(progress)/100, ImVec2(0.0f, 0.0f));
+               //      ImGui::Text("calculate fifteen min acerage");
+               //      ImGui::SameLine();
+               //      ImGui::ProgressBar(float(progress)/100, ImVec2(0.0f, 0.0f));
+               // }
 
-               ImGui::SliderFloat("ROM", &rom, 0.0f, 100.0f);
-               ImGui::SameLine();
-               ImGui::Text("%d/%d Go", int(freeSpace / 1024 / 1024 / 1024), int(totalSpace / 1024 / 1024 /1024));
 
-               ImGui::SliderInt("process", &proci, 0, procimax);
+               ImGui::Indent();
+               ImGui::Text("debug");
+               ImGui::Text("loadavg: %s", procloadavg.c_str());
+               ImGui::Text("stat: %s", procstat.c_str());
 
-               ImGui::Text("%s", loadLine.c_str());
-               ImGui::Text("%f", cpUsage);
-               ImGui::Text("01-minute Load Average: %10.2f", loadAvg[0]);
-               ImGui::Text("05-minute Load Average: %10.2f", loadAvg[1]);
-               ImGui::Text("15-minute Load Average: %10.2f", loadAvg[2]);
-               ImGui::Text("05-minute Load Average: %10.2f", loadAvg[3]);
-               ImGui::Text("15-minute Load Average: %10.2f", loadAvg[4]);
-               ImGui::Text("15-minute Load Average: %10.2f", loadAvg[5]);
-               ImGui::Text("15-minute Load Average: %10.2f", loadAvg[6]);
+               ImGui::Text("%f", cpufreq);
 
-               ImGui::PlotLines("process", plotData, IM_ARRAYSIZE(plotData));
-               ImGui::Unindent();
+               ImGui::Checkbox("Demo Window", &show_demo_window);
+               ImGui::Checkbox("Another Window", &show_another_window);
+//               ImGui::Unindent();
 
+               if (ImGui::IsKeyDown(ImGuiKey_Q)) ImGui::Text("yeah");
+               if (poopen && ImGui::Button("Close this window")) boopen = false;
                ImGui::End();
           }
 
@@ -314,6 +465,7 @@ int main(int, char**)
           if (show_another_window)
           {
                ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+             
                ImGui::Text("Hello from another window!");
                if (ImGui::Button("Close Me"))
                     show_another_window = false;
@@ -341,6 +493,10 @@ int main(int, char**)
      }
 
      // Cleanup
+     delete bufcpufreq;
+     delete bufappfreq;
+     delete bufupfreq;
+
      ImGui_ImplOpenGL2_Shutdown();
      ImGui_ImplGlfw_Shutdown();
      ImGui::DestroyContext();
