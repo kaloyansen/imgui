a211d3c84ffa2e33ccdd49becf841e3e3a19fbb7
kaloyanski <kaloyanski@users.noreply.github.com>
2024-03-09 11:48:41 +0000

    deploy: 459b1563f425f3dbe84dba00b62b4a9146ef2ba0

--- search/all_0.js
+++ search/all_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['about_0',['about',['../index.html#autotoc_md1',1,'']]],
-  ['and_20em_20manjaro_20em_1',['tested on &lt;em&gt;poky&lt;/em&gt;, &lt;em&gt;opensuse&lt;/em&gt;, &lt;em&gt;debian&lt;/em&gt; and &lt;em&gt;manjaro&lt;/em&gt;',['../index.html',1,'']]],
-  ['and_20run_2',['build and run',['../index.html#autotoc_md4',1,'']]]
+  ['and_20em_20manjaro_20em_1',['tested on &lt;em&gt;poky&lt;/em&gt;, &lt;em&gt;opensuse&lt;/em&gt;, &lt;em&gt;debian&lt;/em&gt; and &lt;em&gt;manjaro&lt;/em&gt;',['../index.html',1,'']]]
 ];
--- search/all_d.js
+++ search/all_d.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['reset_0',['reset',['../classK3Buffer.html#a4a9f6b18be7ced332286997e8cb931d6',1,'K3Buffer']]],
-  ['run_1',['build and run',['../index.html#autotoc_md4',1,'']]]
+  ['reset_0',['reset',['../classK3Buffer.html#a4a9f6b18be7ced332286997e8cb931d6',1,'K3Buffer']]]
 ];
