071c7345cdd8d5594f17ebfb732d741461d81f4b
kaloyanski <kaloyanski@users.noreply.github.com>
2024-03-09 11:14:10 +0000

    deploy: 8d06b0af43a301bb3adf18fc75b1d8df5114b025

--- search/all_0.js
+++ search/all_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['and_20em_20manjaro_20em_0',['tested on: &lt;em&gt;poky&lt;/em&gt;, &lt;em&gt;opensuse&lt;/em&gt;, &lt;em&gt;debian&lt;/em&gt; and &lt;em&gt;manjaro&lt;/em&gt;',['../index.html',1,'']]],
-  ['and_20run_1',['build and run',['../index.html#autotoc_md4',1,'']]]
+  ['about_0',['about',['../index.html#autotoc_md1',1,'']]],
+  ['and_20em_20manjaro_20em_1',['tested on: &lt;em&gt;poky&lt;/em&gt;, &lt;em&gt;opensuse&lt;/em&gt;, &lt;em&gt;debian&lt;/em&gt; and &lt;em&gt;manjaro&lt;/em&gt;',['../index.html',1,'']]],
+  ['and_20run_2',['build and run',['../index.html#autotoc_md4',1,'']]]
 ];
