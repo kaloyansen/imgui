574abe3f572ce112b217f086ad05c5c4224bb9ac
kaloyanski <kaloyanski@users.noreply.github.com>
2024-03-14 08:24:24 +0000

    deploy: 0ba8bebd8be6a5ef0a418daa2fe3eaf4167ae704

--- search/all_2.js
+++ search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['code_0',['code',['../index.html#autotoc_md5',1,'']]],
-  ['connect_1',['connect',['../classK3System.html#a2c8987fc30498749094e8a3070ddbcb8',1,'K3System']]]
+  ['connect_1',['connect',['../classK3System.html#a2c8987fc30498749094e8a3070ddbcb8',1,'K3System']]],
+  ['contact_2',['contact',['../index.html#autotoc_md6',1,'']]]
 ];
