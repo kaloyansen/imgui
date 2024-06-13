96a402af0e612a0f45fc319c370c41e632120f27
kaloyansen <kalojanssen@gmail.com>
2024-06-13 13:52:23 +0300

    heat

--- heat/src/heat.c
+++ heat/src/heat.c
@@ -63,7 +63,54 @@ ImU32 Couleur(const char * colorName)
 }
 
 
+void StyleSeaborn() {
 
+     ImPlotStyle& style              = ImPlot::GetStyle();
+
+     ImVec4* colors                  = style.Colors;
+     colors[ImPlotCol_Line]          = IMPLOT_AUTO_COL;
+     colors[ImPlotCol_Fill]          = IMPLOT_AUTO_COL;
+     colors[ImPlotCol_MarkerOutline] = IMPLOT_AUTO_COL;
+     colors[ImPlotCol_MarkerFill]    = IMPLOT_AUTO_COL;
+     /* colors[ImPlotCol_ErrorBar]      = ImVec4(0.00f, 0.00f, 0.00f, 1.00f); */
+     /* colors[ImPlotCol_FrameBg]       = ImVec4(1.00f, 1.00f, 1.00f, 1.00f); */
+     /* colors[ImPlotCol_PlotBg]        = ImVec4(0.92f, 0.92f, 0.95f, 1.00f); */
+     /* colors[ImPlotCol_PlotBorder]    = ImVec4(0.00f, 0.00f, 0.00f, 0.00f); */
+     colors[ImPlotCol_LegendBg] = ImVec4(0.0f, 0.0f, 0.0f, 0.0f);
+     /* colors[ImPlotCol_LegendBorder]  = ImVec4(0.80f, 0.81f, 0.85f, 1.00f); */
+     /* colors[ImPlotCol_LegendText]    = ImVec4(0.00f, 0.00f, 0.00f, 1.00f); */
+     /* colors[ImPlotCol_TitleText]     = ImVec4(0.00f, 0.00f, 0.00f, 1.00f); */
+     /* colors[ImPlotCol_InlayText]     = ImVec4(0.00f, 0.00f, 0.00f, 1.00f); */
+     /* colors[ImPlotCol_AxisText]      = ImVec4(0.00f, 0.00f, 0.00f, 1.00f); */
+     /* colors[ImPlotCol_AxisGrid]      = ImVec4(1.00f, 1.00f, 1.00f, 1.00f); */
+     /* colors[ImPlotCol_AxisBgHovered]   = ImVec4(0.92f, 0.92f, 0.95f, 1.00f); */
+     /* colors[ImPlotCol_AxisBgActive]    = ImVec4(0.92f, 0.92f, 0.95f, 0.75f); */
+     /* colors[ImPlotCol_Selection]     = ImVec4(1.00f, 0.65f, 0.00f, 1.00f); */
+     /* colors[ImPlotCol_Crosshairs]    = ImVec4(0.23f, 0.10f, 0.64f, 0.50f); */
+
+     style.LineWeight       = 1.5;
+     style.Marker           = ImPlotMarker_None;
+     style.MarkerSize       = 4;
+     style.MarkerWeight     = 1;
+     style.FillAlpha        = 1.0f;
+     style.ErrorBarSize     = 5;
+     style.ErrorBarWeight   = 1.5f;
+     style.DigitalBitHeight = 8;
+     style.DigitalBitGap    = 4;
+     style.PlotBorderSize   = 0;
+     style.MinorAlpha       = 1.0f;
+     style.MajorTickLen     = ImVec2(0,0);
+     style.MinorTickLen     = ImVec2(0,0);
+     style.MajorTickSize    = ImVec2(0,0);
+     style.MinorTickSize    = ImVec2(0,0);
+     style.MajorGridSize    = ImVec2(1.2f,1.2f);
+     style.MinorGridSize    = ImVec2(1.2f,1.2f);
+     style.PlotPadding      = ImVec2(12,12);
+     style.LabelPadding     = ImVec2(5,5);
+     style.LegendPadding    = ImVec2(5,5);
+     style.MousePosPadding  = ImVec2(5,5);
+     style.PlotMinSize      = ImVec2(300,225);
+}
 
 const char* formatString(const char *format, ...)
 {
@@ -193,6 +240,7 @@ int main(int, char**)
      io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
 
      ImGui::StyleColorsClassic();
+     StyleSeaborn();
      
      ImGui_ImplGlfw_InitForOpenGL(window, true);
      ImGui_ImplOpenGL2_Init();
